004b12e6e173be2a2100b5310e9091f69892638e
Rogelio Martinez <rogejr89@gmail.com>
2024-09-26 14:17:40 -0400

    setting up the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react'
-
 import './App.css'
+
+import ContactList from './components/ContactList'
+
+//i think we need a head for top title 
+//need the <thead>.<tr>.<th> this is the top row
+//acording to instuction we need a head, row, and column componets
 
 function App() {
   
   return (
     <>
-    <h1>Here</h1>
+      <ContactList />
     </>
   )
 }
